8cde91b3b28c55012dee744f3b7accc55dada866
khangrey <khan.grey.dev@gmail.com>
2022-04-13 12:47:21 +0600

    Fix css and js files.

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="css/app.css">
         @livewireStyles
         <!-- Scripts -->
     </head>
@@ -26,7 +26,7 @@
                 @yield('content')
             </main>
         </div>
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="js/app.js" defer></script>
         @livewireScripts
     </body>
 </html>
