364f3ff0e2e477a396622eb3687b7c397590a5d7
khangrey <khan.grey.dev@gmail.com>
2022-09-06 17:19:42 +0600

    Yandex webmaster verification.

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="yandex-verification" content="dd424fad718a1de8" />
 
     <title>Sirius Messenger</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
