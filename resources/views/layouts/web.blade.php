e87104f66e075166bfc920c78baca0d3234aed17
khangrey <khan.grey.dev@gmail.com>
2022-09-08 14:10:30 +0600

    Google tag manager edited.

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -5,11 +5,22 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-VG4LQC8K5W"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
         gtag('js', new Date());
 
         gtag('config', 'G-VG4LQC8K5W');
     </script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TW24JZM');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -29,6 +40,12 @@
     @flasher_render
 </head>
 <body class="font-sans antialiased bg-white text-base">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TW24JZM"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <x-web.header />
     <main>
         {{ $slot }}
