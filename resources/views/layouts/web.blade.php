edb1b0335552851a91b664da9f5123b570263d9a
khangrey <khan.grey.dev@gmail.com>
2022-09-22 15:22:19 +0600

    Split testing completed.

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -47,34 +47,25 @@
             }
         });
 
-        (function (m, e, t, r, i, k, a) {
-            m[i] =
-                m[i] ||
-                function () {
-                    (m[i].a = m[i].a || []).push(arguments);
-                };
-            var z = null;
-            m[i].l = 1 * new Date();
-            for (var j = 0; j < document.scripts.length; j++) {
-                if (document.scripts[j].src === r) {
-                    return;
-                }
-            }
-            (k = e.createElement(t)),
-                (a = e.getElementsByTagName(t)[0]),
-                (k.async = 1),
-                (k.src = r),
-                a.parentNode.insertBefore(k, a);
-        })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-        ym(90270514, 'init', {
-            clickmap: true,
-            trackLinks: true,
-            accurateTrackBounce: true,
-            webvisor: true,
-            params: yaParams,
+        ym(90516163, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            params:yaParams
         });
     </script>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/90516163" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
     <noscript><div><img src="https://mc.yandex.ru/watch/90270514" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <!-- /Yandex.Metrika counter -->
 </head>
